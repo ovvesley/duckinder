71e6cf923cece253ca2f48de18322b1168b86c21
vvesly <wsf.ley@gmail.com>
2019-09-01 01:01:02 -0300

    add template_index.php

---
+++ template/template_index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php title()?></title>
+    <link rel="stylesheet" href="lib/css/bootstrap.min.css">
+    <link rel="stylesheet" href="lib/css/mdb.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+     <button>
+    </nav>
+    <script src="lib/js/jquery-3.4.1.min.js"></script>
+    <script src="lib/js/bootstrap.min.js"></script>
+    <script src="lib/js/popper.min.js"></script>
+    <script src="lib/js/mdb.min.js"></script>
+</body>
+</html>
