655435a2393263faf9019a73830e2b933d0be98a
vvesly <wsf.ley@gmail.com>
2019-09-01 14:18:46 -0300

    redirecionamento para a error.

---
+++ error.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login | Duckinder</title>
+    <link rel="stylesheet" href="lib/css/bootstrap.min.css">
+    <link rel="stylesheet" href="lib/css/mdb.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="text-center ">
+        <h1 style="font-size:800%;">Deu ERRO!</h1>
+        <h2><i class="zmdi zmdi-mood-bad"></i></h2>
+        </div>
+
+    </div>
+
+
+
+    <script src="lib/js/jquery-3.4.1.min.js"></script>
+    <script src="lib/js/bootstrap.min.js"></script>
+    <script src="lib/js/popper.min.js"></script>
+    <script src="lib/js/mdb.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+
+</html>
